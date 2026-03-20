773b191e7497d39d5a7c7bd1a2a5aa4ecb5e24d6
eryck regis da silva <eryckengsoftware@gmail.com>
2026-03-20 20:23:43 -0300

    project created

---
+++ src/main/java/com/eryckregis/dscommerce/DscommerceApplication.java
@@ -0,0 +1,13 @@
+package com.eryckregis.dscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscommerceApplication.class, args);
+	}
+
+}
