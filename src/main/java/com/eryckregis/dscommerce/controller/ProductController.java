b33f0c90651a6c192d686069cbcbd30c29122f47
eryck regis da silva <eryckengsoftware@gmail.com>
2026-09-09 16:08:59 -0300

    modifying created service, controller, repository, and DTO

--- src/main/java/com/eryckregis/dscommerce/controller/ProductController.java
+++ src/main/java/com/eryckregis/dscommerce/controller/ProductController.java
@@ -9,17 +9,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/products")
+@RequestMapping(value = "products")
 public class ProductController {
 
     @Autowired
-    private ProductService productService;
+    private ProductService service;
 
     @GetMapping(value = "/{id}")
-    public ProductDTO findById(@PathVariable Long id){
-        ProductDTO dto = productService.findById(id);
-        return dto;
-
+    public ProductDTO findById(@PathVariable Long id) {
+        return service.findById(id);
     }
+
 
 }
--- src/main/java/com/eryckregis/dscommerce/service/ProductService.java
+++ src/main/java/com/eryckregis/dscommerce/service/ProductService.java
@@ -7,21 +7,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
 @Service
 public class ProductService {
 
     @Autowired
-    private ProductRepository Repository;
+    private ProductRepository repository;
 
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
-        Optional<Product> result = Repository.findById(id);
-        Product product = result.get();
-        ProductDTO dto = new ProductDTO(product);
-        return dto;
-
+        Product product = repository.findById(id).get();
+        return new ProductDTO(product);
     }
+
 
 }
